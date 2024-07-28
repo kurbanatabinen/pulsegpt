8e42ae099d103b78dbad970e21a0ee1f0a498fd1
Kurban <kurban.at@gmail.com>
2024-07-28 08:50:47 +0200

    1.0.1 Fix

--- config/pulsegpt.php
+++ config/pulsegpt.php
@@ -4,5 +4,5 @@ return [
     'model' => env('PULSEGPT_MODEL', 'gpt-3.5-turbo'),
     'role' => env('PULSEGPT_ROLE', 'user'),
     'prompt' => env('PULSEGPT_PROMPT', 'In one sentence, give me a higly motivational developer quote'),
-    'cache' => env('PULSEGPT_CACHE', 300), // In milliseconds
+    'cache' => env('PULSEGPT_CACHE', 60), // In seconds
 ];
